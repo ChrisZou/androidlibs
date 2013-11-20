f0e68b02598b4f6c8032660471b063bd9bc0346d
ZouYong <zouyong@doodlemobile.com>
2013-11-20 17:15:58 +0800

    Added Log and toast helper; Removed some unused folders; Added some style code

---
+++ src/com/chriszou/androidlibs/Toaster.java
@@ -0,0 +1,10 @@
+package com.chriszou.androidlibs;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+	public static void s(Context context, int textResId) {
+		Toast.makeText(context, textResId, Toast.LENGTH_SHORT).show();
+	}
+}
