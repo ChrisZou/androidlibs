4758df3c18887220b768cb7bd7424e8392dbf679
Zou,Yong <zoumengdie@gmail.com>
2014-08-22 11:48:55 +0800

    Add style definition and toast short method

--- src/com/chriszou/androidlibs/Toaster.java
+++ src/com/chriszou/androidlibs/Toaster.java
@@ -7,4 +7,8 @@ public class Toaster {
 	public static void s(Context context, int textResId) {
 		Toast.makeText(context, textResId, Toast.LENGTH_SHORT).show();
 	}
+
+	public static void s(Context context, String msg) {
+		Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+	}
 }
