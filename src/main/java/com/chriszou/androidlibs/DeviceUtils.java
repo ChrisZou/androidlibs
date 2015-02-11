a7408f8a5653580f230098a39a01344590df17ed
ZouYong <zouyong@zhibodang.com>
2015-02-11 15:36:14 +0800

    Add a method dpToPixel in DeviceUtils

--- src/main/java/com/chriszou/androidlibs/DeviceUtils.java
+++ src/main/java/com/chriszou/androidlibs/DeviceUtils.java
@@ -1,6 +1,8 @@
 package com.chriszou.androidlibs;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 
 /**
  * Created by zouyong on 10/31/14.
@@ -20,4 +22,12 @@ public class DeviceUtils {
     public static int screenHeight(Context context) {
         return context.getResources().getDisplayMetrics().heightPixels;
     }
+
+
+    public static int dpToPixel(Context context, float dip) {
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        int length = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, dm);
+        return length;
+    }
+
 }
