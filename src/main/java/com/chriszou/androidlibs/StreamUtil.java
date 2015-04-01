b37357675670309ef501f2352adab6e548e2ca2d
ZouYong <zouyong@zhibodang.com>
2015-04-01 12:17:44 +0800

    Added a stream-to-string util that could close the inputstream

--- src/main/java/com/chriszou/androidlibs/StreamUtil.java
+++ src/main/java/com/chriszou/androidlibs/StreamUtil.java
@@ -20,14 +20,20 @@ public class StreamUtil {
      * @throws IOException
      */
 	public static String getString(InputStream is) throws IOException {
+        return getString(is, false);
+	}
+
+    public static String getString(InputStream in, boolean close) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
         int length = 0;
-        while ((length = is.read(buffer)) != -1) {
+        while ((length = in.read(buffer)) != -1) {
             baos.write(buffer, 0, length);
         }
         String result = baos.toString();
         baos.close();
+        if(close) in.close();
         return result;
-	}
+    }
+
 }
