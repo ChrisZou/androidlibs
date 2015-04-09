61113c706fe7367a9d274160767e26a9c9d1e06b
ZouYong <zouyong@zhibodang.com>
2015-04-09 16:07:35 +0800

    Add TextViewBinder

---
+++ src/main/java/com/chriszou/androidlibs/Hashable.java
@@ -0,0 +1,9 @@
+package com.chriszou.androidlibs;
+
+/**
+ * Created by Chris on 4/3/15.
+ */
+public interface Hashable {
+    void prepareHash();
+    String get(String key);
+}
---
+++ src/main/java/com/chriszou/androidlibs/TextViewBinder.java
@@ -0,0 +1,61 @@
+package com.chriszou.androidlibs;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Chris on 3/19/15.
+ */
+public class TextViewBinder {
+    public static final String INTERPOLATION_START = "#{";
+    public static final String INTERPOLATION_END = "}";
+
+    public static void bindViewsUsingTag(Hashable hash, View view) {
+        if (hash==null || view==null) return;
+        if (view instanceof TextView) {bindText(hash, view); return;}
+        if (! (view instanceof ViewGroup)) return;
+
+        ViewGroup viewGroup = (ViewGroup) view;
+        for (int i = 0; i < viewGroup.getChildCount(); i++) {
+            View v = viewGroup.getChildAt(i);
+            if(v instanceof ViewGroup) {
+                bindViewsUsingTag(hash, v);
+            } else if (v instanceof TextView) {
+                bindText(hash, v);
+            }
+        }
+    }
+
+    private static void bindText(Hashable hash, View view) {
+        if (hash==null || view==null || !(view instanceof TextView)) return;
+
+        Object tagObj = view.getTag();
+        if (tagObj==null || !(tagObj instanceof String)) return;
+
+        final String tag = (String) tagObj;
+        final String key = getKey(tag);
+        String value = hash.get(key);
+        String text = hasInterpolation(tag) ? getFormatedText(tag, key, value) : value;
+        if (text==null) return;
+        ((TextView)view).setText(text);
+    }
+
+    private static String getFormatedText(String tag, String key, String value) {
+        if (tag==null || value ==null || key==null) return null;
+        return tag.replace(INTERPOLATION_START+key+INTERPOLATION_END, value);
+    }
+
+    //Get the string inclosed by "#{" and "}" as the key, if no #{ and } pair found，then return the string itself
+    //For example: pass "#{hello}， world" should return hello, pass "hello, world" should return "hello, world"
+    private static String getKey(String tag) {
+        if (tag==null) return "N/A";
+        if(hasInterpolation(tag)) { return tag.substring(tag.indexOf(INTERPOLATION_START)+2, tag.indexOf(INTERPOLATION_END, tag.indexOf(INTERPOLATION_START))); }
+        return tag;
+    }
+
+    public static boolean hasInterpolation(String tag) {
+        if (tag==null) return false;
+        return tag.contains(INTERPOLATION_START) && tag.indexOf(INTERPOLATION_END, tag.indexOf(INTERPOLATION_START)) > 0;
+    }
+}
